51f89893b289c9d9588e9a2df6e2964c19a7e951
Awynimantha <awynimantha@gmail.com>
2024-09-24 23:43:59 +0530

    added chunker

--- java/main/copier/Application.java
+++ java/main/copier/Application.java
@@ -1,4 +1,6 @@
 import files.*;
+import workers.Disassembler;
+
 import java.io.IOException;
 
 
@@ -6,8 +8,10 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) {
         try{
-            File file = new File("/home/yasiru/Desktop/test.c","");
-            System.out.println(file.getSize());
+            File file = new File("/home/yasiru/Downloads/1.mp4","");
+            System.out.println(file.getContent());
+            Disassembler disassembler = new Disassembler(file.getContent());
+            System.out.println(disassembler.getChunks());
         } catch (IOException e) {
             System.out.println(e);
         }
--- java/main/copier/workers/Disassembler.java
+++ java/main/copier/workers/Disassembler.java
@@ -2,6 +2,7 @@ package workers;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import configurations.config;
 
@@ -11,8 +12,8 @@ public class Disassembler {
     public int chunks;
     
     public Disassembler(ArrayList<Integer> content){
-        content = content;
-        chunks = setChunks();
+        this.content = content;
+        this.chunks = setChunks();
     }
 
     public int setChunks() {
@@ -25,8 +26,17 @@ public class Disassembler {
         return whole_chunks;
     }
 
-    public HashMap<Integer, ArrayList<Integer>> getChunks() {
-        
+    public HashMap<Integer, List<Integer>> getChunks() {
+       int l_boundary = 0;
+       int size = content.size();
+       HashMap<Integer, List<Integer>> chunks = new HashMap<Integer, List<Integer>>();
+       int i = 0;
+       for( i = 0; i!=this.chunks-1; i++) {
+            chunks.put(i, content.subList(l_boundary, l_boundary + config.CHUNK_SIZE));
+            l_boundary = l_boundary + config.CHUNK_SIZE;
+       }
+       chunks.put(i, content.subList(l_boundary, size));
+       return chunks;
     }
 
 }
